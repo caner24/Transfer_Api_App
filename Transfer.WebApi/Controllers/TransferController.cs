9415cc1098852041d704bb9655514ab3785a6b0e
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2023-08-07 15:05:37 +0300

    Add project files.

---
+++ Transfer.WebApi/Controllers/TransferController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Transfer.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferController : Controller
+    {
+
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
