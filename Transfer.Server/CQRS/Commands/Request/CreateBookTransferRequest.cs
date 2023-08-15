02b67d8e7c4c6e696ea51d71e094f2bbafab5994
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2023-08-15 14:05:30 +0300

    Added Post Method

--- Transfer.Server/CQRS/Commands/Request/CreateBookTransferRequest.cs
+++ Transfer.Server/CQRS/Commands/Request/CreateBookTransferRequest.cs
@@ -4,16 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Transfer.Client.Request;
+using Transfer.Client.Response;
 using Transfer.Server.CQRS.Commands.Response;
 
 namespace Transfer.Server.CQRS.Commands.Request
 {
-    public class CreateBookTransferRequest : IRequest<CreateBookTransferResponse>
+    public class CreateBookTransferRequest : IRequest<TransferServiceSearchResponse>
     {
-        public string VehicleIds { get; set; }
-        public int UserId { get; set; }
-        public int Adults { get; set; }
-        public int Children { get; set; }
+        
+        public class Root
+        {
+            public List<string> VehicleIds { get; set; }
+            public int Adults { get; set; }
+            public int UserID { get; set; }
+            public int Children { get; set; }
+            public List<Tag> Tags { get; set; }
+        }
 
+        public class Tag
+        {
+            public string Key { get; set; }
+            public string Value { get; set; }
+        }
     }
 }
